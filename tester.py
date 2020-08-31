2b7cde0d52a36cb9a4f1440b29cdc21a2f872bc4
Alberto Fanton <alberto.fanton@protonmail.com>
2020-08-31 22:43:38 +0200

    Updates

--- tester.py
+++ tester.py
@@ -7,16 +7,20 @@ from gens import gen_custom_files, gen_public_files
 SEED = 0
 
 
+CREATE = False
+PUBLIC_TESTS = False
+EXECUTE_DEBUG = True
+
 if __name__ == "__main__":
     rnd.seed(SEED)
 
     # TEST CREATION
-    create_custom_tests() if False else None
+    create_custom_tests() if CREATE else None
 
     # PUBLIC TEST
-    execute_public() if True else None
+    execute_public() if PUBLIC_TESTS else None
 
     # COMPLETE DEBUG TEST
-    gen1 = gen_public_files() if False else None
-    gen2 = gen_custom_files() if False else None
-    execute_debug(gen1, gen2) if False else None
+    gen1 = gen_public_files() if EXECUTE_DEBUG else None
+    gen2 = gen_custom_files() if EXECUTE_DEBUG else None
+    execute_debug(gen1, gen2) if EXECUTE_DEBUG else None
